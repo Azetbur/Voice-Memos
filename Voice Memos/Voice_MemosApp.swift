cb187d960fe43f6c6fbe5b470fddc852ba3fdbf4
Jindrich Kocman <jindra.kocman@gmail.com>
2022-08-21 16:46:06 +0200

    Initial Commit

---
+++ Voice Memos/Voice_MemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_MemosApp.swift
+//  Voice Memos
+//
+//  Created by Jindrich Kocman on 21.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Voice_MemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Voice MemosUITests/Voice_MemosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Voice_MemosUITestsLaunchTests.swift
+//  Voice MemosUITests
+//
+//  Created by Jindrich Kocman on 21.08.2022.
+//
+
+import XCTest
+
+class Voice_MemosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
